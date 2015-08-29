461c43eb9316b9f4ba7e795cb75c2b078c517c18
Steve Kossouho <artscoop93.info@gmail.com>
2015-08-29 14:32:38 +0200

    Python 3 changes

--- maintenance/admin.py
+++ maintenance/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from models import Maintenance, MaintenanceFilter
+from .models import Maintenance, MaintenanceFilter
 
 class MaintenanceFilterInline(admin.TabularInline):
     model = MaintenanceFilter
